10fc3d8be7d4ddb67c96aa2fe5e340e726c39eda
Yu Feng <yfeng28@ur.rochester.edu>
2022-09-26 14:03:24 -0700

    add buffer monitor class

--- sim_infra.py
+++ sim_infra.py
@@ -45,3 +45,91 @@ class ReservationBoard(object):
 	def release_hw_unit(self, sw_stage, hw_unit):
 		self.reservation_board[hw_unit] = False
 		self.occupation_board.pop(hw_unit, None)
+
+
+class BufferMonitor(object):
+	"""docstring for BufferMonitor"""
+	def __init__(self, buffer_list):
+		super(BufferMonitor, self).__init__()
+		self.buffer_list = buffer_list
+		# store the R/W port config
+		self.total_read_port = {}
+		self.total_write_port = {}
+		# initialize the R/W port status
+		self.occupied_read_port = {}
+		self.occupied_write_port = {}
+		for buffer in buffer_list:
+			self.total_read_port[buffer] = buffer.read_port
+			self.total_write_port[buffer] = buffer.write_port
+			self.occupied_read_port[buffer] = 0
+			self.occupied_write_port[buffer] = 0
+
+	# check if there is any available read port for this buffer
+	def check_buffer_available_read_port(self, buffer):
+		if buffer is None:
+			return True
+
+		print(buffer)
+		assert self.occupied_read_port[buffer] <= self.total_read_port[buffer], \
+			"occupied_read_port cannot be greater than total_read_port"
+
+		# occupied read port equals to total read port, then, no available read port
+		if self.occupied_read_port[buffer] == self.total_read_port[buffer]:
+			return False
+		else:
+			return True
+
+	# request a certain number of read port, after request, it increments occupied_read_port
+	# 	* buffer: buffer class
+	# 	* num_port: number of read ports are needed
+	# return:
+	# 	it returns number of ports that can be satisfied
+	# 	this number <= num_port
+	def request_read_port(self, buffer, num_port):
+		# check if the request is zero. if zero, return directly
+		if num_port == 0:
+			return 0
+		avail_port = self.total_read_port[buffer] - self.occupied_read_port[buffer]
+		self.occupied_read_port[buffer] += avail_port
+		if avail_port > num_port:
+			return num_port
+		else:
+			return avail_port
+
+	# check if there is any available write port for this buffer
+	def check_buffer_available_write_port(self, buffer):
+		assert self.occupied_write_port[buffer] <= self.total_write_port[buffer], \
+			"occupied_write_port cannot be greater than total_write_port"
+
+		# occupied write port equals to total write port, then, no available write port
+		if self.occupied_write_port[buffer] == self.total_write_port[buffer]:
+			return False
+		else:
+			return True
+
+	# request a certain number of write port, after request, it increments occupied_write_port
+	# 	* buffer: buffer class
+	# 	* num_port: number of write ports are needed
+	# return:
+	# 	it returns number of ports that can be satisfied
+	# 	this number <= num_port
+	def request_write_port(self, buffer, num_port):
+		avail_port = self.total_write_port[buffer] - self.occupied_write_port[buffer]
+		self.occupied_write_port[buffer] += avail_port
+		if avail_port > num_port:
+			return num_port
+		else:
+			return avail_port
+
+	# refresh the port number after each cycle
+	def refresh_port_status(self):
+		for k in self.occupied_read_port.keys():
+			self.occupied_read_port[k] = 0
+		for k in self.occupied_write_port.keys():
+			self.occupied_write_port[k] = 0
+
+
+
+
+
+		
