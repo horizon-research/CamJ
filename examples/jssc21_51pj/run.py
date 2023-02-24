ec821a29130b6a232e7471180df4ee6726beadbc
Yu Feng <yfeng28@ur.rochester.edu>
2023-02-24 16:00:21 -0500

    tweaks

--- examples/jssc21_51pj/run.py
+++ examples/jssc21_51pj/run.py
@@ -10,7 +10,6 @@ sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 # import local modules
 from camj.sim_core.launch import launch_simulation
-from camj.functional_core.launch import launch_functional_simulation
 
 from examples.jssc21_51pj.mapping import mapping_function
 from examples.jssc21_51pj.sw import sw_pipeline
