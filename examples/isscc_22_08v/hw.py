04c3b1b69434b5798974d69a078314d850f1a9fa
Yu Feng <yfeng28@ur.rochester.edu>
2023-02-28 16:55:58 -0500

    update isscc 22 0.8v

--- examples/isscc_22_08v/hw.py
+++ examples/isscc_22_08v/hw.py
@@ -24,42 +24,39 @@ def hw_config():
     }
 
     fifo_buffer1 = FIFO(
-        name="FIFO1",
-        hw_impl = "sram",
-        count = 2*1,
-        clock = 500,    # MHz
-        write_energy = 3,
-        read_energy = 1,
+        name = "FIFO-1",
+        size = 2 * 42,
         location = ProcessorLocation.COMPUTE_LAYER,
-        duplication = 42,
+        write_energy_per_word = 3,
+        read_energy_per_word = 1,
+        write_word_length = 1,
+        read_word_length = 1, 
         write_unit = "ADC",
         read_unit = "MPUnit"
     )
     hw_dict["memory"].append(fifo_buffer1)
 
     fifo_buffer2 = FIFO(
-        name="FIFO2",
-        hw_impl = "sram",
-        count = 21*8,
-        clock = 500,    # MHz
-        write_energy = 3,
-        read_energy = 1,
+        name = "FIFO-2",
+        size = 21 * 21 * 8,
         location = ProcessorLocation.COMPUTE_LAYER,
-        duplication = 21,
+        write_energy_per_word = 3,
+        read_energy_per_word = 1,
+        write_word_length = 1,
+        read_word_length = 1,
         write_unit = "MPUnit",
         read_unit = "FCUnit",
     )
     hw_dict["memory"].append(fifo_buffer2)
 
     fifo_buffer3 = FIFO(
-        name="FIFO3",
-        hw_impl = "sram",
-        count = 2,
-        clock = 500,    # MHz
-        write_energy = 3,
-        read_energy = 1,
+        name = "FIFO-3",
+        size = 2,
         location = ProcessorLocation.COMPUTE_LAYER,
-        duplication = 1,
+        write_energy_per_word = 3,
+        read_energy_per_word = 1,
+        write_word_length = 1,
+        read_word_length = 1,
         write_unit = "FCUnit",
         read_unit = None
     )
@@ -67,23 +64,21 @@ def hw_config():
 
     adc = ADC(
         name = "ADC",
-        output_throughput = (42, 1, 1),
+        output_per_cycle = (2, 1, 1),
         location = ProcessorLocation.SENSOR_LAYER,
     )
     adc.set_output_buffer(fifo_buffer1)
     hw_dict["compute"].append(adc)
 
     mp_unit = ComputeUnit(
-        name="MPUnit",
-        domain=ProcessDomain.DIGITAL,
-        location=ProcessorLocation.COMPUTE_LAYER,
-        input_throughput = [(42, 2, 1)],
-        output_throughput = (21, 1, 1),
-        clock = 500, # MHz
-        energy = 3*21*compute_op_power,
+        name = "MPUnit",
+        domain = ProcessDomain.DIGITAL,
+        location = ProcessorLocation.COMPUTE_LAYER,
+        input_per_cycle = [(2, 2, 1)],
+        output_per_cycle = (1, 1, 1),
+        energy_per_cycle = 2 * compute_op_power,
+        num_of_stages = 2,
         area = 10,
-        initial_delay = 0,
-        delay = 1,
     )
     mp_unit.set_input_buffer(fifo_buffer1)
     mp_unit.set_output_buffer(fifo_buffer2)
@@ -93,13 +88,11 @@ def hw_config():
         name="FCUnit",
         domain=ProcessDomain.DIGITAL,
         location=ProcessorLocation.COMPUTE_LAYER,
-        input_throughput = [(21, 21, 8)],
-        output_throughput = (1, 1, 1),
-        clock = 500, # MHz
-        energy = 3*21*compute_op_power,
+        input_per_cycle = [(21, 21, 8)],
+        output_per_cycle = (1, 1, 1),
+        energy_per_cycle = 21 * 21 * 8 * compute_op_power,
+        num_of_stages = 21 * 21 * 8,
         area = 10,
-        initial_delay = 0,
-        delay = 1,
     )
     fc_unit.set_input_buffer(fifo_buffer2)
     fc_unit.set_output_buffer(fifo_buffer3)
