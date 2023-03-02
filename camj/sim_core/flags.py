cef72f03fc133c1362cad0531a04dccc4b0192ae
Yu Feng <yfeng28@ur.rochester.edu>
2023-03-02 15:06:16 -0500

    tweaks

--- camj/sim_core/flags.py
+++ camj/sim_core/flags.py
@@ -3,6 +3,6 @@ ENABLE_DEBUG = False
 # enable verbal printing
 ENABLE_VERBAL = True
 # print information for every XX cycles if ENABLE_VERBAL is True
-PRINT_CYCLE = 100
+PRINT_CYCLE = 1000
 # maximum running cycle count
-MAX_CYCLE_CNT = 100000000
+MAX_CYCLE_CNT = 10000000
