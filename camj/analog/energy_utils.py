bac22686b0e80bf9c23b0fdbb7e2876f8d87a598
Tianrui Ma <terrymarth@gmail.com>
2023-03-21 09:15:04 -0500

    update energy_utils comments

--- camj/analog/energy_utils.py
+++ camj/analog/energy_utils.py
@@ -17,6 +17,9 @@ def gm_id(
             bandwidth (float): amplifier's bandwidth.
             differential (bool): if using differential-input amplifier or single-input amplifier.
             inversion_level (str): 'weak', 'moderate', or 'strong'. It describes the inversion level of the transistors in the amplifier.
+        
+        Returns:
+            [drain current (id), transconductance (gm)]
     """
 
     if inversion_level == 'strong':
@@ -25,6 +28,8 @@ def gm_id(
         gm_id_ratio = 16
     elif inversion_level == 'weak':
         gm_id_ratio = 20
+    else:
+        raise Exception("Defined inversion_level is not supported.")
     num_branch = np.where(differential, 2, 1)
     gm = 2 * np.pi * load_capacitance * gain * bandwidth
     id = gm / gm_id_ratio * num_branch  # [A]
@@ -37,14 +42,20 @@ def get_pixel_parasitic(
         tech_node,  # [nm]
         pitch  # [um]
     ):
-    """ Compute parasitic capacitance on the pixel's output node.
+    """ Compute column parasitic capacitance (Farad per column) on the pixel's output node.
 
-        The parasitic capacitance usually dominates the load capacitance if the pixel's readout circuitry is placed beside the pixel array.
+        This parasitic capacitance usually dominates the load capacitance if the pixel's follow-up circuitry (e.g., readout or processing) is placed at the column level.
+        Based on "(2019, JSSC) A Data-Compressive 1.5/2.75-bit Log-Gradient QVGA Image Sensor With Multi-Scale Readout for Always-On Object Detection" where the
+        parasitic capacitance is 9fF/pixel under 130nm process node and 5um pixel pitch, this model assumes the parasitic capacitance scales with process node,
+        pixel pitch, and pixel array's vertical size (i.e., the number of pixels in one column).
 
         Args:
             array_v (int): vertical size of the pixel array.
             tech_node (int): pixel's process node.
             pitch (float): pixel's pitch size.
+
+        Returns:
+            parasitic capacitance
     """
 
     C_p = 9e-15 / 130 / 5 * tech_node * pitch * array_v
@@ -56,13 +67,16 @@ def get_nominal_supply(tech_node):
 
         Args:
             tech_node (int): pixel's process node.
+
+        Returns:
+            supply voltage
     """
 
     if 130 < tech_node <= 180:
         supply = 1.8
-    if 65 < tech_node <= 130:
+    elif 65 < tech_node <= 130:
         supply = 1.5
-    if tech_node <= 65:
+    elif tech_node <= 65:
         supply = 1.1
     else:
         raise Exception("Defined tech_node is not supported.")
@@ -70,6 +84,15 @@ def get_nominal_supply(tech_node):
 
 
 def parallel_impedance(impedance_array):
+    """ Compute parallel impedance of an array of input impedance.
+
+        Args:
+            impedance_array (array, float): input impedance array.
+
+        Returns:
+            parallel impedance
+    """
+     
     impedance = np.reciprocal(np.sum(np.reciprocal(impedance_array)))
     return impedance
 
@@ -80,7 +103,23 @@ def get_delay(
         current_stage_output_capacitance,
         next_stage_input_capacitance
     ):
-    # 5*Tau represents charging to 99% of the full voltage from 0
+    """ Compute analog component delay.
+
+        This model assumes the delay of an analog component is 5 times of the RC constant at the analog component's output node.
+        The total impedance (R) is the sum of the output impedance of this analog component and the input impedance of the following analog component.
+        The total capacitance (C) is the sum of the output capacitance of this analog component and the input capacitance of the following analog component.
+        5 times of the RC constant represents the latency required by charging the total capacitance to 99% of the nodal voltage swing.
+
+        Args:
+            current_stage_output_impedance (float): output impedance of the current analog component.
+            next_stage_input_impedance (float): output impedance of the next analog component.
+            current_stage_output_capacitance (float): output capacitance of the current analog component.
+            next_stage_input_capacitance (float): output capacitance of the next analog component.
+
+        Returns:
+            delay
+    """
+
     delay = 5 * (parallel_impedance([current_stage_output_impedance, next_stage_input_impedance])) * \
             (current_stage_output_capacitance + next_stage_input_capacitance)
     return delay
